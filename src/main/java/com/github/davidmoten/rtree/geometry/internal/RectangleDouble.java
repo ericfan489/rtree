52e4c6ac93c37ec91b88198b95ce8741f7943355
ericfan489 <55415731+ericfan489@users.noreply.github.com>
2021-08-13 12:12:45 -0400

    Update RectangleDouble.java

--- src/main/java/com/github/davidmoten/rtree/geometry/internal/RectangleDouble.java
+++ src/main/java/com/github/davidmoten/rtree/geometry/internal/RectangleDouble.java
@@ -1,76 +1,61 @@
-package com.github.davidmoten.rtree.geometry.internal;
+package com.github.davidmoten.rtreemulti.geometry.internal;
 
-import com.github.davidmoten.guavamini.Objects;
-import com.github.davidmoten.guavamini.Optional;
+import java.util.Arrays;
+
 import com.github.davidmoten.guavamini.Preconditions;
-import com.github.davidmoten.rtree.geometry.Geometry;
-import com.github.davidmoten.rtree.geometry.Rectangle;
-import com.github.davidmoten.rtree.internal.util.ObjectsHelper;
+import com.github.davidmoten.rtreemulti.geometry.Geometry;
+import com.github.davidmoten.rtreemulti.geometry.Rectangle;
+import com.github.davidmoten.rtreemulti.internal.util.ObjectsHelper;
 
 public final class RectangleDouble implements Rectangle {
-    private final double x1, y1, x2, y2;
 
-    private RectangleDouble(double x1, double y1, double x2, double y2) {
-        Preconditions.checkArgument(x2 >= x1);
-        Preconditions.checkArgument(y2 >= y1);
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+    private final double[] mins;
+    private final double[] maxes;
+
+    private RectangleDouble(double[] mins, double[] maxes) {
+        Preconditions.checkArgument(mins.length == maxes.length);
+        for (int i = 0; i < mins.length; i++) {
+            Preconditions.checkArgument(maxes[i] >= mins[i]);
+        }
+        this.mins = mins;
+        this.maxes = maxes;
     }
 
-    public static RectangleDouble create(double x1, double y1, double x2, double y2) {
-        return new RectangleDouble((double) x1, (double) y1, (double) x2, (double) y2);
-    }
-
-    @Override
-    public double x1() {
-        return x1;
-    }
-
-    @Override
-    public double y1() {
-        return y1;
-    }
-
-    @Override
-    public double x2() {
-        return x2;
-    }
-
-    @Override
-    public double y2() {
-        return  y2;
+    public static RectangleDouble create(double[] mins, double[] maxes) {
+        return new RectangleDouble(mins, maxes);
     }
 
     @Override
     public Rectangle add(Rectangle r) {
-        return new RectangleDouble(min(x1, r.x1()), min(y1, r.y1()), max(x2, r.x2()),
-                max(y2, r.y2()));
+        double[] a = new double[mins.length];
+        double[] b = new double[mins.length];
+        for (int i = 0; i < a.length; i++) {
+            // TODO minor perf improvement - use if
+            a[i] = min(mins[i], r.min(i));
+            b[i] = max(maxes[i], r.max(i));
+        }
+        return new RectangleDouble(a, b);
     }
 
     @Override
-    public boolean contains(double x, double y) {
-        return x >= x1 && x <= x2 && y >= y1 && y <= y2;
+    public boolean contains(double... p) {
+        Preconditions.checkArgument(mins.length == p.length);
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] < mins[i] || p[i] > maxes[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean intersects(Rectangle r) {
-        if (r instanceof RectangleDouble) {
-            RectangleDouble rd = (RectangleDouble) r;
-            return intersects(rd);
-        } else {
-            return GeometryUtil.intersects(x1, y1, x2, y2, r.x1(), r.y1(), r.x2(), r.y2());
-        }
-    }
-
-    private boolean intersects(RectangleDouble rd) {
-        return GeometryUtil.intersects(x1, y1, x2, y2, rd.x1, rd.y1, rd.x2, rd.y2);
+        return GeometryUtil.intersects(mins, maxes, r.mins(), r.maxes());
     }
 
     @Override
     public double distance(Rectangle r) {
-        return GeometryUtil.distance(x1, y1, x2, y2, r.x1(), r.y1(), r.x2(), r.y2());
+        return GeometryUtil.distance(mins, maxes, r.mins(), r.maxes());
     }
 
     @Override
@@ -80,31 +65,32 @@ public final class RectangleDouble implements Rectangle {
 
     @Override
     public String toString() {
-        return "Rectangle [x1=" + x1 + ", y1=" + y1 + ", x2=" + x2 + ", y2=" + y2 + "]";
+        return "Rectangle [mins=" + Arrays.toString(mins) + ", maxes=" + Arrays.toString(maxes) + "]";
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(x1, y1, x2, y2);
+        int result = 1;
+        result = 31 * result + Arrays.hashCode(mins);
+        result = 31 * result + Arrays.hashCode(maxes);
+        return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-        Optional<RectangleDouble> other = ObjectsHelper.asClass(obj, RectangleDouble.class);
-        if (other.isPresent()) {
-            return Objects.equal(x1, other.get().x1) && Objects.equal(x2, other.get().x2)
-                    && Objects.equal(y1, other.get().y1) && Objects.equal(y2, other.get().y2);
+        RectangleDouble other = ObjectsHelper.asClass(obj, RectangleDouble.class);
+        if (other != null) {
+            return Arrays.equals(mins, other.mins) && Arrays.equals(maxes, other.maxes);
         } else
             return false;
     }
 
     @Override
-    public double intersectionArea(Rectangle r) {
+    public double intersectionVolume(Rectangle r) {
         if (!intersects(r))
             return 0;
         else {
-            return create(max(x1, r.x1()), max(y1, r.y1()), min(x2, r.x2()), min(y2, r.y2()))
-                    .area();
+            return create(GeometryUtil.max(mins, r.mins()), GeometryUtil.min(maxes, r.maxes())).volume();
         }
     }
 
@@ -128,18 +114,42 @@ public final class RectangleDouble implements Rectangle {
     }
 
     @Override
-    public double perimeter() {
-        return 2 * (x2 - x1) + 2 * (y2 - y1);
+    public double surfaceArea() {
+        double sum = 0;
+        for (int i = 0; i < mins.length; i++) {
+            double product = 1;
+            for (int j = 0; j < mins.length; j++) {
+                if (i != j) {
+                    product *= maxes[j] - mins[j];
+                }
+            }
+            sum += product;
+        }
+        return 2 * sum;
     }
 
     @Override
-    public double area() {
-        return (x2 - x1) * (y2 - y1);
+    public double volume() {
+        double v = 1;
+        for (int i = 0; i < mins.length; i++) {
+            v *= maxes[i] - mins[i];
+        }
+        return v;
     }
 
     @Override
-    public boolean isDoublePrecision() {
-        return true;
+    public double[] mins() {
+        return mins;
+    }
+
+    @Override
+    public double[] maxes() {
+        return maxes;
+    }
+
+    @Override
+    public int dimensions() {
+        return mins.length;
     }
 
 }
