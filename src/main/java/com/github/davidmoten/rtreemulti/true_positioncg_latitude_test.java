9d6ee488e0a125c04b7ee29ad1c8eecc501892d2
ericfan489 <55415731+ericfan489@users.noreply.github.com>
2021-08-13 14:51:08 -0400

    Update true_positioncg_latitude_test.java

--- src/main/java/com/github/davidmoten/rtreemulti/true_positioncg_latitude_test.java
+++ src/main/java/com/github/davidmoten/rtreemulti/true_positioncg_latitude_test.java
@@ -23,29 +23,30 @@ public class true_positioncg_latitude_test {
 
     // Variables that can be changed to modify how the tree is built
 
-    // max children per node
+    // User input max children per node
     private static int maxChildren=64;
 
-    // min children per node
+    // User input min children per node
     private static int minChildren=32;
 
-    //number of dimensions to build the tree
+    // User input number of dimensions to build the tree
     private static int dimensions=2;
 
-    // names of the attributes to use as dimensions to build the tree
+    // User input names of the attributes to use as dimensions to build the tree
     private static List<String> dimension_attributes = new ArrayList<String>(Arrays.asList("true_positioncg_n", "true_positioncg_e"));
 
-    // name of the attribute used to calculate linear sum and square sum
+    // User input name of the attribute used to calculate linear sum and square sum
     private String data =  "fl_friction_noisy";
 
-    // String query to get data from the database
+    // User input String query to get data from the database
     private String query1 = "SELECT true_positioncg_n, true_positioncg_e, fl_friction_noisy FROM friction_measurement_uml_query_test ORDER BY true_positioncg_n, true_positioncg_e LIMIT 300000";
 
-    // name of the file that the output is saved to
+    // User input name of the file that the output is saved to
     public static String save_file_name = "true_positioncg_latitude_test.csv";
 
     public  List<Entry<Object, Point>> connect() {
         Connection con = null;
+        // initialize list to store data points
         List<Entry<Object, Point>> result = new ArrayList<Entry<Object, Point>>();
         try {
             con = DriverManager.getConnection(url, user, password);
@@ -57,8 +58,7 @@ public class true_positioncg_latitude_test {
             long executionTime = 0;
             long start = System.currentTimeMillis();
 
-            //String query1 = "SELECT true_positioncg_latitude, true_positioncg_longitude, true_positioncg_altitude, fl_friction_noisy, fr_friction_noisy, rl_friction_noisy, rr_friction_noisy FROM friction_measurement_uml_query_test LIMIT 10000";
-            //String query1 ="SELECT true_positioncg_latitude, true_positioncg_longitude, true_positioncg_altitude, fl_friction_noisy, fr_friction_noisy, rl_friction_noisy, rr_friction_noisy FROM friction_measurement_uml_query_test LIMIT 100";
+           
             ResultSet rs1 = stmt.executeQuery(query1);
 
             while (rs1.next()) {
@@ -66,9 +66,9 @@ public class true_positioncg_latitude_test {
                 for(int x = 0; x < dimension_attributes.size(); x++){
                     points.add(rs1.getDouble(dimension_attributes.get(x)));
                 }
+                //store data points in list
                 result.add(Entry.entry(rs1.getDouble(data),Point.create(points)));
-                //result.add(Entry.entry(rs1.getDouble("fl_friction_noisy"), Point.create(rs1.getDouble("true_positioncg_latitude"),rs1.getDouble("true_positioncg_longitude"),rs1.getDouble("true_positioncg_altitude"),rs1.getDouble("fl_friction_noisy"),rs1.getDouble("fr_friction_noisy"),rs1.getDouble("rl_friction_noisy"),rs1.getDouble("rr_friction_noisy"))));
-                //result.add(Entry.entry(rs1.getDouble("fl_friction_noisy"), Point.create(rs1.getDouble("true_positioncg_latitude"),rs1.getDouble("true_positioncg_longitude"))));
+               
             }
             System.out.println("results queried...");
 
@@ -81,48 +81,36 @@ public class true_positioncg_latitude_test {
 
             // Close the connection object
             con.close();
-
-            //System.out.println("Connected to the PostgreSQL server successfully.");
+            
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
+        //return the resulting list of data points
         return result;
     }
-    //These are only the dimensions that the tree will be built upon
-    private static final List<Entry<Object, Point>> dimension_entries = MainTreeData.entriesList();
-    //These are all the attributes in the data
-    private static final List<Entry<Object, Point>> attribute_entries = MainTreeData.entriesList2();
 
     //total number of data points
     private static int size=10000;
 
-    //max nr of children
-
-    //Create an R* tree with the dimension entries. Change the maxChildren, minChildren, dimensions as required.
-    //private static final RTree<Object, Point> tree = RTree.maxChildren(4).minChildren(2).dimensions(2).star().<Object, Point>create().add(dimension_entries);
-
     //For testing purposes, the result will be saved in .csv file on IntelliJ
     public static void main(String[] args) {
 
-        // The below output will be saved in a .csv file
-        // IntelliJ->Run->Configurations->Logs->Save console output to .csv file
-        //System.out.println(tree.asString3(tree, attribute_entries));
+        // Initialize a database driver object and use it to run the function
         true_positioncg_latitude_test temp = new true_positioncg_latitude_test();
         List<Entry<Object, Point>>attributes = temp.connect();
         RTree<Object, Point> test_tree = RTree.maxChildren(maxChildren).minChildren(minChildren).dimensions(dimensions).star().<Object,Point>create().add(attributes);
 
-        //Iterable<Entry<Object, Point>> search_test = test_tree.search(Rectangle.create(0,-80,0,0,0,0,0,50,0,1,1,1,1,1));
+        // Run a search for all entries within the tree and print them to the screen
         Iterable<Entry<Object, Point>> search_test = test_tree.entries();
         Iterator my_iterator = search_test.iterator();
         while(my_iterator.hasNext()) {
             Object element = my_iterator.next();
             System.out.print(element + " ");
         }
-        //System.out.println(test_tree.asString(test_tree));
-        //Creates the rectangles representation in the target folder.
+
         test_tree.visualize(600, 600).save("target/test_location300000_rtree_" + maxChildren + "_data.png");
         try {
+            //create file to write output to
             File myObj = new File(save_file_name);
             if (myObj.createNewFile()) {
                 System.out.println("File created successfully.");
@@ -135,6 +123,7 @@ public class true_positioncg_latitude_test {
         }
 
         try {
+            //write the output of the rtree to a file
             FileWriter myWriter = new FileWriter(save_file_name);
             myWriter.append(test_tree.asString(test_tree));
             myWriter.flush();
