f4bbc81d906622a824d9fb2ae5d086ea489e2ed3
ericfan489 <55415731+ericfan489@users.noreply.github.com>
2021-08-13 12:02:06 -0400

    Update Point.java

--- src/main/java/com/github/davidmoten/rtree/geometry/Point.java
+++ src/main/java/com/github/davidmoten/rtree/geometry/Point.java
@@ -1,9 +1,31 @@
-package com.github.davidmoten.rtree.geometry;
+package com.github.davidmoten.rtreemulti.geometry;
+
+import java.util.List;
+
+import com.github.davidmoten.rtreemulti.geometry.internal.PointDouble;
 
 public interface Point extends Rectangle {
 
-    double x();
-
-    double y();
+    double[] mins();
+    
+    default double[] maxes() {
+        return mins();
+    }
+    
+    default double[] values() {
+        return mins();
+    }
+    
+    public static Point create(double... x) {
+        return PointDouble.create(x);
+    }
+    
+    public static Point create(List<? extends Number> x) {
+        double[] a = new double[x.size()];
+        for (int i = 0; i< x.size(); i++) {
+            a[i] = x.get(i).doubleValue();
+        }
+        return create(a);
+    }
 
 }
